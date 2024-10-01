1293b50e3af1038f947a64ac43ce1bfbd39f26ea
Fabian Streit <fabian@ekmor.com>
2024-10-01 22:18:52 +0200

    setup next.js

---
+++ provi-frontend/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center content-center justify-center h-dvh w-dvw">
+      <div className="p-5 rounded-lg shadow-xl hover:bg-gray-200 bg-gray-50">
+        Welcome to the ProVi Frontend
+      </div>
+    </div>
+  );
+}
