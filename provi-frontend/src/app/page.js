d5845b4e89a4c25e23f37011c21d7a397a282953
Fabian Streit <fabian@ekmor.com>
2024-10-02 23:13:06 +0200

    create prelimn. folder structure

--- provi-frontend/src/app/page.js
+++ provi-frontend/src/app/page.js
@@ -1,9 +1,5 @@
+import ExampleComponent from "@/components/exampleComponent";
+
 export default function Home() {
-  return (
-    <div className="flex items-center content-center justify-center h-dvh w-dvw">
-      <div className="p-5 rounded-lg shadow-xl hover:bg-gray-200 bg-gray-50">
-        Welcome to the ProVi Frontend
-      </div>
-    </div>
-  );
+  return <ExampleComponent title="Welcome, to the ProVi Frontend!" />;
 }
