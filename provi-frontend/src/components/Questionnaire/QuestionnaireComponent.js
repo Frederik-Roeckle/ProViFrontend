760da3f1b9d9024f418b0c2ae3cba17a030d6f99
Fabian Streit <fabian@ekmor.com>
2024-12-30 17:13:07 +0100

    minor changes to allow actions to work

--- provi-frontend/src/components/Questionnaire/QuestionnaireComponent.js
+++ provi-frontend/src/components/Questionnaire/QuestionnaireComponent.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-import Link from 'next/link';
-// import { CTooltip } from '@coreui/react'; not needed yes could be left out i think
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import Link from "next/link";
+import { CTooltip } from "@coreui/react"; // not needed yes could be left out i think
 // import AlertPopup from './AlertPopup';  not possible need to be done on the mainpage and not in the subcomponent
 
 const questionsCsv = `Question ID,Question Text,Answer Type,Options
@@ -37,15 +37,14 @@ const questionsCsv = `Question ID,Question Text,Answer Type,Options
 //                                                                if answer no -> skip next question (adds 2 to index)
 // exampple base: id:23, follow-up question: id:24
 
-
 const QuestionnaireComponent = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [currentAnswer, setCurrentAnswer] = useState('');
+  const [currentAnswer, setCurrentAnswer] = useState("");
   const [answers, setAnswers] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
-  // parses the question csv to json 
+  // parses the question csv to json
   useEffect(() => {
     const parseQuestions = () => {
       Papa.parse(questionsCsv, {
@@ -53,17 +52,19 @@ const QuestionnaireComponent = () => {
         skipEmptyLines: true,
         complete: (result) => {
           setQuestions(result.data);
-          setAnswers(new Array(result.data.length).fill(''));
+          setAnswers(new Array(result.data.length).fill(""));
         },
       });
     };
     parseQuestions();
   }, []);
 
-
   // handles the next question button, saves answers
   const handleNextQuestion = () => {
-    if (currentAnswer === '' || (Array.isArray(currentAnswer) && currentAnswer.length === 0)) {
+    if (
+      currentAnswer === "" ||
+      (Array.isArray(currentAnswer) && currentAnswer.length === 0)
+    ) {
       alert("Please answer the question.");
       return;
     }
@@ -73,21 +74,19 @@ const QuestionnaireComponent = () => {
     setAnswers(updatedAnswers);
 
     // logic for follow-up queation if No -> skip the next question
-    if(
-      currentQuestion['Answer Type'] === 'follow-up question' &&
-      currentAnswer === 'No'
+    if (
+      currentQuestion["Answer Type"] === "follow-up question" &&
+      currentAnswer === "No"
     ) {
       setCurrentQuestionIndex(currentQuestionIndex + 2);
-      setCurrentAnswer('');
+      setCurrentAnswer("");
       return;
-      
     }
 
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setCurrentAnswer(answers[currentQuestionIndex + 1] || ''); // Pre-fill answer if already answered
+      setCurrentAnswer(answers[currentQuestionIndex + 1] || ""); // Pre-fill answer if already answered
     }
-
   };
 
   // for question 17 => method handling the order selection + function to count the already used numbers (no double numbers allowed)
@@ -98,33 +97,36 @@ const QuestionnaireComponent = () => {
 
   const getUsedNumbers = () => Object.values(currentAnswer);
 
-
   const currentQuestion = questions[currentQuestionIndex];
 
   return (
-   
-    <div className="bg-white p-6 shadow-md rounded-md h-full w-full flex flex-col gap-4">
-
+    <div className="flex flex-col w-full h-full gap-4 p-6 bg-white rounded-md shadow-md">
       {currentQuestion ? (
-        <div className="flex flex-col gap-4 h-full">
-          <h2 className="text-lg font-semibold">Question {currentQuestionIndex + 1} of {questions.length}</h2>
+        <div className="flex flex-col h-full gap-4">
+          <h2 className="text-lg font-semibold">
+            Question {currentQuestionIndex + 1} of {questions.length}
+          </h2>
           <div className="flex items-center gap-2">
             <p>
-              {currentQuestion['Question Text'].split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  {index < currentQuestion['Question Text'].split('\n').length - 1 && <br />}
-                </React.Fragment>
-              ))}
+              {currentQuestion["Question Text"]
+                .split("\n")
+                .map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    {index <
+                      currentQuestion["Question Text"].split("\n").length -
+                        1 && <br />}
+                  </React.Fragment>
+                ))}
             </p>
-            {currentQuestion['Tooltip'] && (
-              <CTooltip content={currentQuestion['Tooltip']} placement="top">
+            {currentQuestion["Tooltip"] && (
+              <CTooltip content={currentQuestion["Tooltip"]} placement="top">
                 <span className="text-sm text-gray-500 cursor-pointer">ℹ️</span>
               </CTooltip>
             )}
           </div>
 
-          {currentQuestion['Answer Type'] === 'text' && (
+          {currentQuestion["Answer Type"] === "text" && (
             <textarea
               value={currentAnswer}
               onChange={(e) => setCurrentAnswer(e.target.value)}
@@ -133,13 +135,13 @@ const QuestionnaireComponent = () => {
               rows={3}
             />
           )}
-          {currentQuestion['Answer Type'] === 'numeric' && (
+          {currentQuestion["Answer Type"] === "numeric" && (
             <input
               type="number"
               value={currentAnswer}
               onChange={(e) => {
                 const value = e.target.value;
-                if (/^\d*$/.test(value)) { 
+                if (/^\d*$/.test(value)) {
                   setCurrentAnswer(value);
                 }
               }}
@@ -148,9 +150,10 @@ const QuestionnaireComponent = () => {
             />
           )}
 
-          {(currentQuestion['Answer Type'] === 'multiple choice' || currentQuestion['Answer Type'] === 'follow-up question') && (
+          {(currentQuestion["Answer Type"] === "multiple choice" ||
+            currentQuestion["Answer Type"] === "follow-up question") && (
             <div className="flex flex-col gap-2">
-              {currentQuestion['Options'].split('|').map((option, index) => (
+              {currentQuestion["Options"].split("|").map((option, index) => (
                 <label key={index} className="flex items-center gap-2">
                   <input
                     type="radio"
@@ -165,17 +168,22 @@ const QuestionnaireComponent = () => {
               ))}
             </div>
           )}
-          {currentQuestion['Answer Type'] === 'multiple select' && (
+          {currentQuestion["Answer Type"] === "multiple select" && (
             <div className="flex flex-col gap-2">
-              {currentQuestion['Options'].split('|').map((option, index) => (
+              {currentQuestion["Options"].split("|").map((option, index) => (
                 <label key={index} className="flex items-center gap-2">
                   <input
                     type="checkbox"
                     name="question-option"
                     value={option}
-                    checked={Array.isArray(currentAnswer) && currentAnswer.includes(option)}
+                    checked={
+                      Array.isArray(currentAnswer) &&
+                      currentAnswer.includes(option)
+                    }
                     onChange={(e) => {
-                      const updatedAnswers = Array.isArray(currentAnswer) ? [...currentAnswer] : [];
+                      const updatedAnswers = Array.isArray(currentAnswer)
+                        ? [...currentAnswer]
+                        : [];
                       if (e.target.checked) {
                         updatedAnswers.push(option);
                       } else {
@@ -193,48 +201,49 @@ const QuestionnaireComponent = () => {
               ))}
             </div>
           )}
-          {currentQuestion['Answer Type'] === 'order question' && (
+          {currentQuestion["Answer Type"] === "order question" && (
             <div className="flex flex-col gap-4">
-              {currentQuestion['Options']
-                .split('|')
-                .map((option, index) => (
-                  <div key={index} className="flex items-center justify-between gap-2">
-                    <p className="flex-grow">{option}</p>
-                    <select
-                      value={currentAnswer[option] || ''}
-                      onChange={(e) =>
-                        handleDropdownChange(e.target.value, option)
-                      }
-                      className="p-2 border rounded-md"
-                    >
-                      <option value="">Order</option>
-                      {[...Array(currentQuestion['Options'].split('|').length)]
-                        .map((_, i) => i + 1)
-                        .map((order) => (
-                          <option key={order} value={order} disabled={getUsedNumbers().includes(
-                            order.toString()
-                          )}>
-                            {order}
-                          </option>
-                        ))}
-                    </select>
-                  </div>
-                ))}
+              {currentQuestion["Options"].split("|").map((option, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-between gap-2"
+                >
+                  <p className="flex-grow">{option}</p>
+                  <select
+                    value={currentAnswer[option] || ""}
+                    onChange={(e) =>
+                      handleDropdownChange(e.target.value, option)
+                    }
+                    className="p-2 border rounded-md"
+                  >
+                    <option value="">Order</option>
+                    {[...Array(currentQuestion["Options"].split("|").length)]
+                      .map((_, i) => i + 1)
+                      .map((order) => (
+                        <option
+                          key={order}
+                          value={order}
+                          disabled={getUsedNumbers().includes(order.toString())}
+                        >
+                          {order}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+              ))}
             </div>
           )}
           <div className="flex justify-end mt-4">
             {currentQuestionIndex < questions.length - 1 ? (
               <button
                 onClick={handleNextQuestion}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                className="px-4 py-2 text-white bg-blue-500 rounded-md"
               >
                 Next Question
               </button>
             ) : (
               <Link href="/endpage" passHref>
-                <button
-                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                >
+                <button className="px-4 py-2 text-white bg-blue-500 rounded-md">
                   Finish
                 </button>
               </Link>
