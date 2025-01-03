35051bc9408d29497e1a18c66760a7f9f4cab70e
Duc-Anh27 <dnguyen27500@gmail.com>
2025-01-03 14:33:01 +0100

    added changes Marie suggested

--- provi-frontend/src/app/endpage/page.js
+++ provi-frontend/src/app/endpage/page.js
@@ -2,18 +2,24 @@
 
 import React from 'react';
 
-const EndPageComponent = () => {
+export default function EndPage() {
   return (
-    <div className="bg-white p-8 shadow-lg rounded-lg h-auto w-3/4 mx-auto flex flex-col gap-8 my-16">
-      <h1 className="text-2xl font-bold">Thank you for participating</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 shadow-lg rounded-lg h-auto w-3/4 mx-auto flex flex-col gap-8 my-16">
+        <h1 className="text-3xl font-bold">Thank you for participating</h1>
+        <p className="text-xl">
+          Thank you for taking the time and participating in this experiment! 
+          Your contribution is crucial to our research on understanding the user perspective of process mining visualizations. 
+          <br />
+          If you have any questions or would like to learn more about our research, please don’t hesitate to contact me at haege@uni-mannheim.de.
+          <br />
+          <br />
+          Best
+          <br />
+          Marie
+        </p>
+      </div>
     </div>
   );
 };
 
-export default EndPageComponent;
---
+++ provi-frontend/src/app/knowledgequestion/page.js
@@ -0,0 +1,141 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Papa from "papaparse";
+import AlertPopup from '../../components/Questionnaire/AlertPopup';
+import ScrollProgressBar from "../../components/WelcomePage/ScrollProgressBar";
+
+import "@coreui/coreui/dist/css/coreui.min.css";
+
+export default function WelcomeComponent() {
+
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+  const [showModal, setShowModal] = useState(false); // Modal visibility
+  const [alertMessage, setAlertMessage] = useState(""); // sets alertMessage
+
+
+  // hardcoded questionnaire from Marie if any changes/ addition want to be done please follow the format of the csv
+  const csvData = `
+      id,type,question,options
+      7,knowledge,What is process mining?,"A data-driven technique that involves cleaning, transforming, and analyzing raw business data to uncover hidden patterns;A method for extracting data from process models to improve workflow efficiency;A method of creating flowcharts and diagrams to represent processes and optimize operations based on interviews and existing documentation;A technique for discovering, monitoring, and improving real processes by extracting knowledge from event logs"
+      8,knowledge,What is a spaghetti process?,"A process that is heavily automated and linear;A process that is complex because of many interconnected subprocesses but still follows a clear structure;A highly complex, unstructured process with many variations and loops"
+      9,knowledge,What is a process variant?,"A specific activity sequence that corresponds to the course of at least one case in the process;A specific activity sequence that represents the 'happy path' and is expected to be followed by all process instances;A specific activity sequence that is part of a larger process"
+      10,knowledge,What are business process models for?,"To track and report the performance of business operations in real-time;To illustrate the organizational hierarchy and reporting structure of an organization;To support organizations in communicating, analyzing, documenting, redesigning, improving, monitoring, or implementing processes"
+      11,knowledge,Which of the following answers is NOT a business process modeling notation?,"Petri Net;UML;BPMN;DFG"
+      12,knowledge,What is a DFG (Directly-Follows-Graph)?,"A diagram showing every possible path in a process;A graphical notation used to represent business processes, including activities, events, and decision points;A graph that displays the sequence of events directly following each other in a process"
+      13,knowledge,Have you ever worked with a DFG?,"Yes;No"
+      `;
+
+      useEffect(() => {
+        const parsedData = Papa.parse(csvData.trim(), { header: true }).data;
+        const formattedQuestions = parsedData.map((question) => ({
+          ...question,
+          options: question.options
+            ? question.options.split(";").map((opt) => opt.trim())
+            : [],
+        }));
+        setQuestions(formattedQuestions);
+      }, []);
+  
+
+      const handleAnswerChange = (e, questionId) => {
+        setAnswers((prevAnswers) => ({
+          ...prevAnswers,
+          [questionId]: e.target.value,
+        }));
+      };
+
+      const handleSubmit = (e) => {
+      
+        const unansweredQuestions = questions.filter((q) => {
+          const answer = answers[q.id];
+          if (q.type === "open") {
+            return !answer || answer.trim() === "";
+          }
+          return !answer;
+        });
+      
+        if (unansweredQuestions.length > 0) {
+          const missingQuestions = unansweredQuestions
+            .map((q, index) => `${questions.indexOf(q) + 1}. ${q.question}`) // Add question number
+            .join("\n");
+      
+          setAlertMessage(
+            `Please answer the following questions before starting the experiment:\n\n${missingQuestions}`
+          );
+          setShowModal(true);
+          return;
+        }
+      
+      };
+      
+  
+  
+
+  return (
+    <div className="bg-gray-50 p-10 shadow-xl rounded-lg h-auto w-3/5 mx-auto flex flex-col gap-10 items-center justify-center">
+      <ScrollProgressBar />
+      <h1 className="text-3xl font-bold">Process Visualization Experiment</h1>
+      
+        <AlertPopup
+        visible={showModal}
+        message={alertMessage}
+        onClose={() => setShowModal(false)}
+       />
+        
+      <div className="flex flex-col gap-6 mt-6">
+        <h2 className="text-3xl font-semibold">Pre-Experiment Questions</h2>
+        {questions.map((q, index) => (
+          <div key={q.id} className="mb-6">
+            <h3 
+              className="font-semibold mb-2 text-2xl" 
+            >
+              {index + 1}. {q.question} {/* Display question number */}
+            </h3>
+            {q.type === "multiple" || q.type === "knowledge" ? (
+              <div>
+                {q.options.map((option, idx) => (
+                  <label key={idx} className="block text-xl">
+                    <input
+                      type="radio"
+                      name={`question-${q.id}`}
+                      value={option}
+                      onChange={(e) => handleAnswerChange(e, q.id)}
+                      className="mr-2"
+                    />
+                    {option}
+                  </label>
+                ))}
+              </div>
+            ) : (
+              <input
+                type="text"
+                name={`question-${q.id}`}
+                placeholder="Your answer"
+                onChange={(e) => handleAnswerChange(e, q.id)}
+                className="border p-2 rounded w-full text-m"
+              />
+            )}
+          </div>
+        ))}
+
+      </div>
+
+      <Link href="/home" passHref>
+        <button
+            type="submit"
+            onClick={(e) => {
+            e.preventDefault(); 
+            handleSubmit(e);
+            }}
+            className="px-10 py-3 rounded-lg mt-4 self-center bg-blue-500 text-white hover:bg-blue-600"
+        >
+            Enter Knowledge Questions
+        </button>
+       </Link>
+      
+    </div>
+  );
+};
